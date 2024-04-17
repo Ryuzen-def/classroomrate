d4e14c185df38a1c31d8d0c5214e6b83eb59778a
MugataSan01 <aikho250@gmail.com>
2024-04-18 01:44:33 +0700

    Tambah kelas

--- add_class.php
+++ add_class.php
@@ -1,23 +1,27 @@
+<?php session_start(); ?>
+<?php include "header.php" ?>
 <?php require_once 'includes/config.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tambah Kelas</title>
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
-  <?php include 'includes/functions.php'; ?>
+<?php include 'includes/functions.php'; ?>
+<div class="content-wrapper" bis_skin_checked="1" style="min-height: 1604.8px;">
+  <section class="content-header">
+    <div class="container-fluid" bis_skin_checked="1">
+      <div class="row mb-2" bis_skin_checked="1">
+        <div class="col-sm-6" bis_skin_checked="1">
+          <h1>Tambah Kelas</h1>
+        </div>
+        <div class="col-sm-6" bis_skin_checked="1">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Tambah Kelas</li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
 
-  <div class="container-fluid">
-    <div class="row">
-      <?php include 'includes/sidebar.php'; ?>
-      <div class="col-md-9 col-lg-10 main-content">
-        <h2>Tambah Kelas</h2>
+  <section class="content">
+    <div class="col-md-9 col-lg-10 main-content card">
+      <div class="card-body">
         <form action="includes/add_class.php" method="post">
           <div class="form-group">
             <label for="name">Nama Kelas</label>
@@ -31,10 +35,6 @@
         </form>
       </div>
     </div>
-  </div>
+</div>
 
-  <!-- Bootstrap JS -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="assets/js/script.js"></script>
-</body>
-</html>
+<?php include "footer.php"?>
