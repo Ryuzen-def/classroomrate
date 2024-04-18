3d337457c196f3ff8d199f3d95ce93e1d5b81b85
Ryuzen-def <abangrianb@gmail.com>
2024-04-18 08:49:27 +0700

    #

--- header.php
+++ header.php
@@ -22,6 +22,9 @@
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="#" class="nav-link">Contact</a>
             </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="add_class.php" class="nav-link">Add Class</a>
+            </li>
         </ul>
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
